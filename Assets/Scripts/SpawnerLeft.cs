743eae41a5af317e2545adf14cfc16fc64e2a426
Shackhal <27marcelovelasquez@gmail.com>
2025-09-13 16:47:55 -0500

    add new version

---
+++ Assets/Scripts/SpawnerLeft.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using TMPro;
+
+public class SpawnerLeft : MonoBehaviour
+{
+    [SerializeField] GameObject[] Enemies;
+    float timer;
+    [SerializeField] float intervalo;
+    [SerializeField] GameObject[] Spawns;
+    bool up;
+    float originalIntervalo;
+    [SerializeField] TextMeshProUGUI timerText;
+
+    void Start()
+    {
+        originalIntervalo = intervalo;
+        Invoke("Spawn", intervalo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; 
+        timerText.text = "Tiempo: " + Mathf.FloorToInt(timer);
+    }
+
+    void Spawn()
+    {
+        int i = UnityEngine.Random.Range(0, Spawns.Length);
+
+        int random = UnityEngine.Random.Range(0, Enemies.Length);
+
+        GameObject temporalEnemy = Instantiate(Enemies[random], Spawns[i].transform.position, Quaternion.identity);
+        temporalEnemy.GetComponent<Rigidbody2D>().linearVelocityX = 3 + (timer / 5);
+        float r = UnityEngine.Random.Range(1, 3);
+        intervalo = r;
+        Invoke("Spawn", intervalo);
+        up = !up;
+
+
+    }
+}
